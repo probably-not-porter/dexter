fb42e1ef27d4d7ebb30d2e3c5e603fc7cc5700a5
probably-not-porter <pelibby16@earlham.edu>
2021-08-10 22:04:26 -0400

    add types

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const color_conv = {
+    "normal": "#A8A77A",
+    "grass": "#7AC74C",
+    "poison": "#A33EA1",
+    "fighting": "#C22E28",
+    "water": "#6390F0",
+    "electric": "#F7D02C",
+    "fairy": "#D685AD",
+    "ground": "#E2BF65",
+    "rock": "#B6A136",
+    "bug": "#A6B91A",
+    "ghost": "#735797",
+    "steel": "#B7B7CE",
+    "dragon": "#6F35FC",
+    "dark": "#705746",
+    "flying": "#A98FF3",
+    "fire": "#EE8130",
+    "psychic": "#F95587",
+    "ice": "#96D9D6"
+}
--- js/main.js
+++ js/main.js
@@ -7,11 +7,33 @@ function load_by_num(n){
     }else{
         $.getJSON('https://pokeapi.co/api/v2/pokemon/' + n, function(data) {
             console.info(data);
+
             var img = data["sprites"]["other"]["official-artwork"]["front_default"];
             var name = data["name"];
+            var types = data["types"];
+
             document.getElementById("sprite").src = img;
             document.getElementById("num").innerText = "#" + n;
             document.getElementById("name").innerText = name.replace(/\b\w/g, l => l.toUpperCase());
+
+            // types
+            let type1 = document.getElementById("type1");
+            let type2 = document.getElementById("type2");
+            type1.style.visibility = "hidden";
+            type2.style.visibility = "hidden";
+            for (let x = 0; x < types.length; x++ ){
+                if (x == 0){
+                    type1.style.visibility = "visible";
+                    type1.style.backgroundColor = color_conv[types[x]["type"]["name"]];
+                    type1.innerHTML = "<div class='slot'>TYPE 1</div>" + "<span class='name'>" + types[x]["type"]["name"].replace(/\b\w/g, l => l.toUpperCase()) + "</span>";
+                }
+                if (x == 1){
+                    type2.style.visibility = "visible";
+                    type2.style.backgroundColor = color_conv[types[x]["type"]["name"]];
+                    type2.innerHTML = "<div class='slot'>TYPE 2</div>" + "<span class='name'>" + types[x]["type"]["name"].replace(/\b\w/g, l => l.toUpperCase()) + "</span>"
+                }
+            }
+
         });
     }
 }
