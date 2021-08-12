3f724cd18ae7d1039a6cb249f1c44ca607580550
probably-not-porter <pelibby16@earlham.edu>
2021-08-12 16:12:54 -0400

    fix search function

--- js/search.js
+++ js/search.js
@@ -1,52 +1,74 @@
+var search_block = false;
+
 $( document ).ready(function() {
     document.getElementById('button_0').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '0';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '0';
+            check_search();
+        }
     });
     
     document.getElementById('button_1').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '1';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '1';
+            check_search();
+        }
     });
     
     document.getElementById('button_2').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '2';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '2';
+            check_search();
+        }
     });
     
     document.getElementById('button_3').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '3';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '3';
+            check_search();
+        }
     });
     
     document.getElementById('button_4').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '4';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '4';
+            check_search();
+        }
     });
     
     document.getElementById('button_5').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '5';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '5';
+            check_search();
+        }
     });
     
     document.getElementById('button_6').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '6';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '6';
+            check_search();
+        }
     });
     
     document.getElementById('button_7').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '7';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '7';
+            check_search();
+        }
     });
     
     document.getElementById('button_8').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '8';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '8';
+            check_search();
+        }
     });
     
     document.getElementById('button_9').addEventListener('click', function (event) {
-        document.getElementById("search").innerText += '9';
-        check_search();
+        if (search_block == false){
+            document.getElementById("search").innerText += '9';
+            check_search();
+        }
     });
 });
 
@@ -55,8 +77,13 @@ $( document ).ready(function() {
 function check_search(){
     let search = document.getElementById("search").innerText;
     if (search.length > 2){
+        search_block = true;
+        if (parseInt(search) > 898 || parseInt(search) < 1){
+            document.getElementById("search").innerText = "ERR"
+        }
         setTimeout(function(){
             document.getElementById("search").innerText = "";
+            search_block = false;
         }, 1000);
         
         load_by_num(parseInt(search));
