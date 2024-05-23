eb35d154b86ec642c6b20e69bc51f691834c5f77
Porter Libby <pelibby16@earlham.edu>
2024-05-23 15:15:50 -0400

    fix searching

--- js/search.js
+++ js/search.js
@@ -6,91 +6,80 @@
 */
 
 $( document ).ready(function() {
+
+    const search_elem = document.getElementById("search");
+    var num = ""
     // each event corrosponds to a digit
     document.getElementById('button_0').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '0';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('0'); }
     });
     
     document.getElementById('button_1').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '1';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('1'); }
     });
     
     document.getElementById('button_2').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '2';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('2'); }
     });
     
     document.getElementById('button_3').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '3';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('3'); }
     });
     
     document.getElementById('button_4').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '4';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('4'); }
     });
     
     document.getElementById('button_5').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '5';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('5'); }
     });
     
     document.getElementById('button_6').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '6';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('6'); }
     });
     
     document.getElementById('button_7').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '7';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('7'); }
     });
     
     document.getElementById('button_8').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '8';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('8'); }
     });
     
     document.getElementById('button_9').addEventListener('click', function (event) {
-        if (search_block == false){
-            document.getElementById("search").innerText += '9';
-            check_search(); // check to see if search is 3 digits long
-        }
+        if (search_block == false){ add_num('9'); }
     });
+
+    function add_num(n){
+        num += n;
+        check_search();
+    }
+
+    function check_search(){ // checks to see if search is complete 3-digit (001, 121, 032)
+        if (num.length == DEX_COUNT.toString().length){
+            if (parseInt(num) > DEX_COUNT || parseInt(num) < 1){
+                search_elem.innerText = "ERR" // if pokemon does not exist, display ERR
+            }
+            else{
+                load_by_num(parseInt(num)); // load search integer (dex num)
+                num = ""
+            }
+            setTimeout(function(){
+                search_elem.innerText = "";
+                num = ""
+            }, 500);
+        }
+        let underscore_len = DEX_COUNT.toString().length - num.length;
+        if (underscore_len == 4){
+            search_elem.innerText = "";
+        }
+        else{
+            search_elem.innerText = num + "_".repeat(underscore_len);
+        }
+        
+    }
 });
 
 
 
-function check_search(){ // checks to see if search is complete 3-digit (001, 121, 032)
-    let search = document.getElementById("search").innerText;
-    if (search.length == DEX_COUNT.toString().length){
-        if (parseInt(search) > DEX_COUNT || parseInt(search) < 1){
-            document.getElementById("search").innerText = "ERR" // if pokemon does not exist, display ERR
-        }
-        else{
-            load_by_num(parseInt(search)); // load search integer (dex num)
-        }
-        setTimeout(function(){
-            document.getElementById("search").innerText = "";
-        }, 500);
-    }
-}
+
